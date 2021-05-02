18e49d31404df1519f77cfd7cb9d7784b23df6c4
Hyeokjin <gurwls1060@naver.com>
2021-05-02 14:24:11 +0900

    객체지향적으로 코드 리팩토링

---
+++ src/chapter01/step02/Audience.java
@@ -0,0 +1,14 @@
+package chapter01.step02;
+
+public class Audience {
+    private final Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        return bag.hold(ticket);
+    }
+
+}
---
+++ src/chapter01/step02/TicketSeller.java
@@ -0,0 +1,13 @@
+package chapter01.step02;
+
+public class TicketSeller {
+    private final TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+    }
+}
