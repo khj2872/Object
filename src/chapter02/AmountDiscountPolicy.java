aa42955c5a50110c18a14e203a0233f6e7a0f374
kang <hykang2872@gmail.com>
2021-05-04 17:52:48 +0900

    DiscountPolicy의 getDiscountAmount 구현

--- src/chapter02/AmountDiscountPolicy.java
+++ src/chapter02/AmountDiscountPolicy.java
@@ -10,6 +10,6 @@ public class AmountDiscountPolicy extends DiscountPolicy {
 
     @Override
     protected Money getDiscountAmount(Screening screening) {
-        return null;
+        return discountAmount;
     }
 }
