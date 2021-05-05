06b1cbeb01b75f4bd74b1d2b0dba31a8a6149bbc
Hyeokjin <gurwls1060@naver.com>
2021-05-05 19:10:13 +0900

    DiscountPolicy interface로 분리

---
+++ src/chapter02/DefaultDiscountPolicy.java
@@ -0,0 +1,24 @@
+package chapter02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class DefaultDiscountPolicy implements DiscountPolicy {
+    private final List<DiscountCondition> conditions;
+
+    public DefaultDiscountPolicy(DiscountCondition ...conditions) {
+        this.conditions = Arrays.asList(conditions);
+    }
+
+    @Override
+    public Money calculateDiscountAmount(Screening screening) {
+        for (DiscountCondition each : conditions) {
+            if (each.isSatisfiedBy(screening)) {
+                return getDiscountAmount(screening);
+            }
+        }
+        return Money.ZERO;
+    }
+
+    protected abstract Money getDiscountAmount(Screening screening);
+}
--- src/chapter02/DiscountPolicy.java
+++ src/chapter02/DiscountPolicy.java
@@ -1,23 +1,5 @@
 package chapter02;
 
-import java.util.Arrays;
-import java.util.List;
-
-public abstract class DiscountPolicy {
-    private final List<DiscountCondition> conditions;
-
-    public DiscountPolicy(DiscountCondition ...conditions) {
-        this.conditions = Arrays.asList(conditions);
-    }
-
-    public Money calculateDiscountAmount(Screening screening) {
-        for (DiscountCondition each : conditions) {
-            if (each.isSatisfiedBy(screening)) {
-                return getDiscountAmount(screening);
-            }
-        }
-        return Money.ZERO;
-    }
-
-    protected abstract Money getDiscountAmount(Screening screening);
+public interface DiscountPolicy {
+    Money calculateDiscountAmount(Screening screening);
 }
